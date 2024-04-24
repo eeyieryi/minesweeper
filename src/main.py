5f06e482f5d6192925bed9bde43c74571ed00749
Emanuel <152207479+eeyieryi@users.noreply.github.com>
2024-04-24 14:19:07 +0400

    add a game class

--- src/main.py
+++ src/main.py
@@ -1,46 +1,32 @@
 import pygame
 
-from constants import GAME_TITLE, GRID_SIZE, WINDOW_SIZE
-from grid import Grid
-from selection import Selection
+from constants import GAME_TITLE, WINDOW_SIZE
+from minesweeper import Minesweeper
 
 
 def run():
     screen = pygame.display.set_mode(WINDOW_SIZE)
     pygame.display.set_caption(GAME_TITLE)
     clock = pygame.time.Clock()
-    running = True
 
-    grid = Grid(size=GRID_SIZE)
-    selection = Selection()
+    game = Minesweeper()
 
-    while running:
+    while game.is_running():
         # poll for events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                game.kill()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
-                    running = False
-                if event.key == pygame.K_h:
-                    selection.go_left()
-                if event.key == pygame.K_l:
-                    selection.go_right()
-                if event.key == pygame.K_k:
-                    selection.go_up()
-                if event.key == pygame.K_j:
-                    selection.go_down()
+                game.handle_keydown_events(event.key)
 
         # draw to screen
         screen.fill("black")
-        grid.draw(screen)
-        selection.draw(screen)
+        game.draw(screen)
 
         pygame.display.flip()
 
         # limit FPS to 60
         clock.tick(60)
-        # running = False
 
 
 if __name__ == "__main__":
---
+++ src/minesweeper.py
@@ -0,0 +1,34 @@
+import pygame
+
+from constants import GRID_SIZE
+from grid import Grid
+from selection import Selection
+
+
+class Minesweeper:
+    def __init__(self) -> None:
+        self._grid = Grid(size=GRID_SIZE)
+        self._selection = Selection()
+        self._running = True
+
+    def handle_keydown_events(self, key: int) -> None:
+        if key == pygame.K_q:
+            self.running = False
+        elif key == pygame.K_h:
+            self._selection.go_left()
+        elif key == pygame.K_l:
+            self._selection.go_right()
+        elif key == pygame.K_k:
+            self._selection.go_up()
+        elif key == pygame.K_j:
+            self._selection.go_down()
+
+    def draw(self, screen: pygame.Surface) -> None:
+        self._grid.draw(screen)
+        self._selection.draw(screen)
+
+    def is_running(self):
+        return self._running
+
+    def kill(self):
+        self._running = False
